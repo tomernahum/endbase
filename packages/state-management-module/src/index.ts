983eaaa9ce048d6760e32eafade390e454498280
tomer <101682824+tomernahum@users.noreply.github.com>
2025-08-27 11:36:08 -0400

    exploratory pseudocode for provider

--- packages/state-management-module/src/index.ts
+++ packages/state-management-module/src/index.ts
@@ -8,5 +8,14 @@ export function createStateManagementModule(
     props: {},
     communication: ReturnType<typeof createCommunicationModuleClient>,
 ) {
-
+    // now we can do a y.js provider here... or a document backed provider or whatever
 }
+
+export function createSimpleYjsProvider(
+    props: {},
+    communication: ReturnType<typeof createCommunicationModuleClient>,
+) {
+    return
+}
+
+// createTextDocumentProvider (moved to separate file)
